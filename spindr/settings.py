ff5befa4facf899f52e53619170d06de0765485d
chaudhary-tushar <tushatstriker@gmail.com>
2024-01-24 03:07:44 +0530

    .env configs for awspindr

--- spindr/settings.py
+++ spindr/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)7nch1#a-prbl%b0-01-!pn$$@olfcrxukni0+jrn8^%)@&2#g'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')])
 
-ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,12 +80,11 @@ WSGI_APPLICATION = 'spindr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'spindrend',
-        'USER': 'romeo7',
-        'PASSWORD': 'goldroger3',
-        'HOST': 'spindrbase.cb2ezp033v93.us-east-1.rds.amazonaws.com',
-        'PORT': '5432'
-
+        'NAME': config('DATABASE_NAME'),
+        'USER': config('DATABASE_USER'),
+        'PASSWORD': config('DATABASE_PASSWORD'),
+        'HOST': config('DATABASE_HOST'),
+        'PORT': config('DATABASE_PORT'),
     }
 }
 
@@ -132,7 +132,7 @@ USE_TZ = True
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 #### MEDIA SETTINGS FOR ACCESSING S3 BUCKET ####
-AWS_STORAGE_BUCKET_NAME = 'spindrbucket'
+AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
 AWS_MEDIA_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com/media'
 
 MEDIA_URL = f'https://{AWS_MEDIA_CUSTOM_DOMAIN}/'
